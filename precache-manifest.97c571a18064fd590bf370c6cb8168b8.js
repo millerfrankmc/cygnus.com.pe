d6d2bdfdba29abf39daac7d52a1104a2996f5277
Frank Miller <fmiller@cygnus.com.pe>
2021-05-04 15:45:16 -0500

    Updates

---
+++ precache-manifest.97c571a18064fd590bf370c6cb8168b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1ddde6622f79055856145d9a4f794f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b29c2a729dfb5f3a3a8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ea15aed0c0241375045c",
+    "url": "/static/js/2.d6f6e888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6f6e888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b29c2a729dfb5f3a3a8",
+    "url": "/static/js/main.9695a3cf.chunk.js"
+  },
+  {
+    "revision": "160ac8fcdf2b43906b3a",
+    "url": "/static/js/runtime-main.42c40c9c.js"
+  }
+]);
